4791ee0843d4dc79bc923abb32b126e70f113ed0
Abhishek Dubey <abhishekdubey331@gmail.com>
2024-09-29 12:13:58 +0530

    chore: Centralize Dependency Management with libs.versions.toml Update (#29)
    * Added impl for libs.version.toml
    
    * Updated PR template

---
+++ app/build.gradle.kts
@@ -0,0 +1,144 @@
+import java.io.FileInputStream
+import java.util.Properties
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.ktlint)
+}
+
+val propFile = rootProject.file("./local.properties")
+val properties = Properties()
+if (propFile.exists()) {
+    properties.load(FileInputStream(propFile))
+}
+val accessKey = properties["ACCESS_KEY"]?.toString() ?: "\"default_key\""
+
+android {
+    compileSdk = 34
+    namespace = "com.unsplash.stockwalls"
+
+    defaultConfig {
+        applicationId = "com.unsplash.stockwalls"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            buildConfigField("String", "ACCESS_KEY", accessKey)
+            isDebuggable = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+
+        getByName("debug") {
+            buildConfigField("String", "ACCESS_KEY", accessKey)
+            isDebuggable = true
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    ktlint {
+        android.set(true)
+        outputToConsole.set(true)
+        ignoreFailures.set(false)
+        reporters {
+            reporter(ReporterType.PLAIN)
+            reporter(ReporterType.CHECKSTYLE)
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    buildFeatures {
+        viewBinding = true
+        compose = true
+        buildConfig = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.kotlinCompilerExtensionVersion.get()
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    // Compose
+    implementation(platform(libs.compose.bom))
+    implementation(libs.hilt.navigation.compose)
+    implementation(libs.compose.material)
+    implementation(libs.compose.foundation)
+    implementation(libs.compose.ui)
+    implementation(libs.material)
+    implementation(libs.compose.ui.tooling.preview)
+    debugImplementation(libs.compose.ui.tooling)
+    androidTestImplementation(platform(libs.compose.bom))
+
+    implementation(libs.compose.material.icons.core)
+    implementation(libs.compose.material.icons.extended)
+
+    implementation(libs.activity.compose)
+    implementation(libs.lifecycle.viewmodel.compose)
+
+    implementation(libs.splashscreen)
+    implementation(libs.kotlin.stdlib.jdk8)
+    implementation(libs.appcompat)
+    implementation(libs.core.ktx)
+    implementation(libs.constraintlayout)
+    implementation(libs.lifecycle.runtime.compose)
+
+    // ViewModel
+    implementation(libs.fragment.ktx)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.moshi)
+    implementation(libs.okhttp.logging.interceptor)
+
+    // Dagger - Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Coil
+    implementation(libs.coil)
+    implementation(libs.coil.compose)
+    implementation(libs.glide.compose)
+
+    // Paging 3
+    implementation(libs.paging.runtime.ktx)
+    implementation(libs.paging.compose)
+
+    // Testing Libraries
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.inline)
+    androidTestImplementation(libs.mockito.android)
+    testImplementation(libs.mockwebserver)
+    testImplementation(libs.truth)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.arch.core.testing)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.junit.ext)
+}
